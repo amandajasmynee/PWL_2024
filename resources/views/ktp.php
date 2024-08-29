4f820cf676292c0cc4cbd4d3f80455a2fa0b2539
amandajasmynee <putriamanda549@gmail.com>
2024-08-29 19:13:42 +0700

    Pertemuan 1 - Kartu Tanda Penduduk

---
+++ resources/views/ktp.php
@@ -0,0 +1,116 @@
+<html>
+
+<head>
+    <title>KARTU TANDA PENDUDUK</title>
+    <link rel="stylesheet" type="text/css" />
+    <style>
+        .first {
+            justify-content: center;
+            align-items: center;
+            display: flex;
+        }
+
+        table {
+            height: 550px;
+            padding: 1em;
+            width: 850px;
+            margin-top: 75px;
+            border: 2px solid;
+            border-radius: 1em;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .img {
+            padding-left: 10px;
+            height: 230px;
+            width: 170px;
+        }
+
+        .alamat {
+            margin-left: 25px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="first">
+        <table>
+            <tr>
+                <th colspan="5" style="font-size: large;font-weight: bold;padding-bottom: 1px;"> PROVINSI JAWA TIMUR</th>
+            </tr>
+            <tr>
+                <th colspan="5" style="font-size: large;font-weight: bold;padding-bottom: 5px;"> KOTA BATU</th>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr>
+                <td style="font-size: large;font-weight: bold;padding-bottom: 5px;">NIK</td>
+                <td style="font-size: large;font-weight: bold;padding-bottom: 5px;">: 3579025406040002</td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Nama</td>
+                <td>: AMANDA JASMYNE BERLIANA PUTRI </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Tempat/Tgl Lahir</td>
+                <td>: BATU, 14-06-2004</td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Jenis Kelamin</td>
+                <td>: PEREMPUAN</td>
+                <div class="goldar">
+                    <td>Gol. Darah :</td>
+                    <td>-</td>
+                </div>
+                <td rowspan="8"></td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Alamat </td>
+                <td>: JL. GAJAH MADA </td>
+            </tr>
+            <tr>
+                <td style="padding-left: 25px; font-weight: bold;">RT/RW</td>
+                <td>: 003/003</td>
+            </tr>
+            <tr>
+                <td style="padding-left: 25px; font-weight: bold;">Kel/Desa</td>
+                <td>: TULUNGREJO </td>
+            </tr>
+            <tr>
+                <td style="padding-left: 25px; font-weight: bold;">Kecamatan</td>
+                <td>: BUMIAJI </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Agama</td>
+                <td>: ISLAM</td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Status Perkawinan</td>
+                <td>: Belum Kawin </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Pekerjaan</td>
+                <td>: PELAJAR/MAHASISWA </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Kewarganegaraan</td>
+                <td>: WNI</td>
+                <td colspan="2"></td>
+                <td style="align-items: center;justify-content: center;display: flex; padding-bottom: 5px;">KOTA BATU</td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Berlaku Hingga</td>
+                <td>: SEUMUR HIDUP</td>
+                <td colspan="2"></td>
+                <td style="align-items: center;justify-content: center;display: flex;">17-09-2021</td>
+            </tr>
+            <tr>
+                <td colspan=4 style="padding-bottom: 50px;"></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
