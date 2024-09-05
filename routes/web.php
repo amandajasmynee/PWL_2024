dea7b0899cc74357539ba7233ec1c7d40eb7a11e
amandajasmynee <putriamanda549@gmail.com>
2024-09-05 23:18:29 +0700

    Pertemuan 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// BASIC ROUTING
+// Nomor 2
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+// Nomor 4
+Route::get('/world', function () {
+    return 'World';
+});
+
+// Nomor 6
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+// Nomor 7
+Route::get('/about', function () {
+    return '2241760081 - AMANDA JASMYNE BERLIANA PUTRI';
+});
+
+// ROUTE PARAMETERS
+// Nomor 8
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
+});
+
+
+// Nomor 11
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' .$postId." Komentar ke: ".$commentId;
+});
+
+
+// Nomor 13
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
+});
+
+// OPTIONAL PARAMETERS
+// Nomor 14
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya ' .$name;
+});
+
+// Nomor 17
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya ' .$name;
 });
